f9b8dca2cf7068750bc2134f2e034e7dfc0b3370
cywooo <jerry3477158@gmail.com>
2022-10-24 01:50:02 +0800

    Update HW01_2_2Layer.py

--- HW001/HW01_2_2Layer.py
+++ HW001/HW01_2_2Layer.py
@@ -56,8 +56,8 @@ np.random.seed()
 
 #參數與矩陣設定
 learning_rate = 1e-6
-epoch = 500
-Num_of_hiden1 = 40
+epoch = 400
+Num_of_hiden1 = 50
 Num_of_outLayer = 2
 batch = 1
 
@@ -90,7 +90,8 @@ def foward(x, w1, w2, b1, b2,n):
 def back( feature,real, w1, w2, b1, b2, l1, l2, a1, a2, rl,n): 
     feature_=np.reshape(feature,(-1,num_of_features))
     real=np.reshape(real,(-1,num_of_targets))
-    Edy=np.reshape(-1*real/(l2.T*np.log(2)),(-1,num_of_targets)) #-1*real/(l2.T*np.log(2)) #(-1*real/l2.T)+((1-real)/(1-l2.T))
+    #Edy=np.reshape((-1*real/l2.T)+((1-real)/(1-l2.T)),(-1,num_of_targets)) #-1*real/(l2.T*np.log(2)) #(-1*real/l2.T)+((1-real)/(1-l2.T))
+    Edy=np.reshape(-1*real/(l2.T*np.log(2)),(-1,num_of_targets))
     
     dw2=np.dot(l1,Edy)
     dw1=np.dot(feature_.T,(np.dot((w2*dReLU(a1)),Edy.T)).T)
